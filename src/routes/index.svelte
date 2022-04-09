f56deff5c52e704bf997ca5126b88a086c6d6f59
Henry Schimke <henry@azule.info>
2022-04-09 17:12:24 +0000

    init and branding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>PlaneTracker</h1>
+<p>PlaneTracker helps you keep track of planes you have flown in.</p>
