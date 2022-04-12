3fb6a60328f64601d580fe0ab12f8c8bb74f9412
Henry Schimke <henry@azule.info>
2022-04-12 20:29:06 +0000

    colors and layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,15 +7,21 @@
 <style>
     h1 {
         color: white;
-        background-color: #333;
+        background-color: #3F3830;
         margin: 0px 0px 0px 0px;
         padding-left: .1em;
+    }
+    div.main {
+        margin-left: 1em;
+        margin-right: 1em;
     }
 </style>
 <h1>Plane Tracker</h1>
 <Nav />
+<div class="main">
 {#if $loginState.email !== "" || $page.url.pathname.startsWith("/login")}
     <slot />
 {:else}
     <Login />
 {/if}
+</div>
