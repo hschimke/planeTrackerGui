c6d96ee5385b10ca2752040e3058f4510a098043
Henry Schimke <henry@azule.info>
2022-04-14 20:58:24 +0000

    build config

--- src/lib/components/login.svelte
+++ src/lib/components/login.svelte
@@ -1,17 +1,6 @@
 <script lang="ts">
-    import { performLogin } from "$lib/login";
     import GoogleLogin from "$lib/components/loginProviders/googleLogin.svelte";
-
-    let email = "";
-
-    async function doLogin() {
-        performLogin(email);
-    }
 </script>
-
-<h2>Login</h2>
-<input bind:value={email} />
-<button on:click={doLogin}>Login</button>
 
 <h2>Google</h2>
 <GoogleLogin />
---
+++ src/lib/components/loginProviders/fakeLogin.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { performLogin } from "$lib/login";
+
+    let email = "";
+
+    async function doLogin() {
+        performLogin(email);
+    }
+</script>
+
+<input bind:value={email} />
+<button on:click={doLogin}>Login</button>
